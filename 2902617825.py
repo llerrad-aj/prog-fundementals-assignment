ea8c5875f6d66476db59ed006545f130c61689f9
llerrad <darrell.linardi@binus.ac.id>
2025-09-01 21:51:08 +0700

    Add files via upload

---
+++ 2902617825.py
@@ -0,0 +1,4 @@
+print ("Nama saya Darrell Aviel Linardi");
+print ("NIM : 2902617825");
+print ("Jurusan Computer Science");
+print ("Bahasa Pemrograman Yang Saya Ingin Dalami Adalah Phyton Karena Mudah Dipelajari");
